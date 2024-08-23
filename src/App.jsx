6a3b1bdbe5bead7532701c06fdcaad83650a7f73
unknown <aaronwan808@gmail.com>
2024-08-23 20:56:20 +0100

    added header file

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './output.css'
+import Header from './Components/Header'
 
 function App() {
   
   return (
-    <div className='bg-slate-700 text-cyan-600 min-h-screen'>APP</div>
+    <div className='bg-slate-700 text-cyan-600 min-h-screen'>
+      <Header></Header>
+    </div>
   )
 }
 
