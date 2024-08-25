7fd4a7693ca8cd4ae07a0bfd8d10fe6be9d032f2
unknown <aaronwan808@gmail.com>
2024-08-25 23:05:18 +0100

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './output.css'
 import Header from './Components/Header'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './Components/Login'
+import BlogList from './Components/BlogList'
 
 function App() {
   const [darkmode , setdarkmode] = useState(true);
 
   return (
+  <Router>
     <div className={`${darkmode ? "bg-slate-700 text-cyan-600 min-h-screen" : "bg-lightmode text-cyan-600 min-h-screen"}`}>
       <Header darkmode={darkmode} setdarkmode={setdarkmode}></Header>
+        <Routes>
+          <Route path='/' element={<BlogList/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+        </Routes>
     </div>
+    </Router>
   )
 }
 
