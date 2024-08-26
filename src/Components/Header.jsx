a2f2d6c273df47426279d7d96c49c13b009b291e
unknown <aaronwan808@gmail.com>
2024-08-26 16:24:07 +0100

    added basic auth

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,51 @@
 import React, { useState } from 'react';
-import { Moon, LogIn, Sun } from 'lucide-react';
+import { Moon, LogIn, Sun, LogOut} from 'lucide-react';
+import { auth } from '../Firebase'
+import { provider } from '../Firebase'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-const Header = ({darkmode, setdarkmode}) => {
+const Header = ({darkmode, setdarkmode, setLoggedIn, loggedIn, username, setUsername, profilePic, setProfilepic}) => {
 
     const handledarkmodeclick = () =>{
         setdarkmode(!darkmode)
         console.log(darkmode);
+    }
+
+    const handlesigninclick =() =>{
+      const auth = getAuth();
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential.accessToken;
+          // The signed-in user info.
+          const user = result.user;
+          setLoggedIn(true);
+          setUsername(user.displayName);
+          setProfilepic(user.photoURL);
+          console.log(user)
+          
+          // IdP data available using getAdditionalUserInfo(result)
+          // ...
+        }).catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          const email = error.customData.email;
+          // The AuthCredential type that was used.
+          const credential = GoogleAuthProvider.credentialFromError(error);
+          // ...
+        });
+    }
+
+    const handlelogout = () =>{
+      const auth = getAuth();
+      signOut(auth).then(() => {
+        setLoggedIn(false);
+      }).catch((error) => {
+        console.log(error);
+      });
     }
     
   return (
@@ -16,20 +56,44 @@ const Header = ({darkmode, setdarkmode}) => {
             Aaron's Blog
           </h1>
           <div className="flex space-x-2">
-            <button
-              className="p-2 rounded-full text-gray-600 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              aria-label="Toggle dark mode"
+            <div className='flex justify-center items-center'>
+              <button
+                className="p-2 rounded-full text-gray-600 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                aria-label="Toggle dark mode"
 
-              onClick={handledarkmodeclick}
-            >
-              {darkmode ? <Moon size={20} /> : <Sun size={20}/>}
-            </button>
-            <button
+                onClick={handledarkmodeclick}
+              >
+                {darkmode ? <Moon size={20} /> : <Sun size={20}/>}
+              </button>
+            </div>
+            
+            <div className='flex justify-center items-center'>
+              {loggedIn ?
+              <button
               className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={handlesigninclick}
             >
-              <LogIn className="mr-2 -ml-1" size={20} />
-              Login
+              <LogOut className="mr-2 -ml-1" size={20} />
+              Logout
             </button>
+              :
+              <button
+                className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={handlesigninclick}
+              >
+                <LogIn className="mr-2 -ml-1" size={20} />
+                Login
+              </button>
+              }
+            </div>
+            <div className="flex items-center space-x-2 bg-opacity-50 bg-gray-700 rounded-full p-1 pr-3">
+                {loggedIn ? (
+                  <img src={profilePic} alt={username} className="w-8 h-8 rounded-full object-cover" />
+                ) : (
+                  <div className='p-2'></div>
+                )}
+                <span className="text-sm font-medium text-white">{username}</span>
+              </div>
           </div>
         </div>
       </div>
---
+++ src/Firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQMb3jSiVYpIe6BV8pb0kjYJOiK-7BT_0",
+  authDomain: "csblog-1aa18.firebaseapp.com",
+  projectId: "csblog-1aa18",
+  storageBucket: "csblog-1aa18.appspot.com",
+  messagingSenderId: "299586585662",
+  appId: "1:299586585662:web:d06924d0a5ee57e9fdbd6f",
+  measurementId: "G-RQRQSSB38F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
